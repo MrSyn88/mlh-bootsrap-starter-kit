9151d872dd2d28d5c6aace7aed994c74540f550f
Nicolas Ruiz <syngenruiz2@gmail.com>
2021-06-28 10:21:16 -0500

    Finishing up

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+/* Here's an example script! You can include anything you'd like to include
+on your webpage in here! Remove the "//" before the alert to have it run
+next time you go to the page! */
+
+//alert("Woah what?!? An Alert!!")
